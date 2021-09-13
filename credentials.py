b2113db1e6e0ecd284655836b61be46fa6e15206
Anthony Amabile <49218550+aamabile001@users.noreply.github.com>
2021-09-12 22:10:13 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,8 +1,8 @@
 class AlpacaConfig:
     # Put your own Alpaca key here:
-    API_KEY = "PKYJX65EM2RK615AW8MM"
+    API_KEY = "**********"
     # Put your own Alpaca secret here:
-    API_SECRET = "ae2BXD73owfZtCuXaVqLCMiY7eYppivue5kWXlo3"
+    API_SECRET = "*********"
     # For paper trading
     ENDPOINT = "https://paper-api.alpaca.markets"
 
